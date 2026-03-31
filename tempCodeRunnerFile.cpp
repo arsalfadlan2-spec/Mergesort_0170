0d82209d8e390999d7934b150c3002b7fed5a1ff
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-03-31 11:15:43 +0700

    Meanambahkan Fungsi menampilkan data

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -44,3 +44,14 @@ void insertionSort() {
         arr[j + 1] = temp; // step 6
     }
 }
+
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
